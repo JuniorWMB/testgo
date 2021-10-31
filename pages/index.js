817502c88afa5fca8168f31ba7304e679eb3c005
Junior <juniormbaya243@gmail.com>
2021-11-01 00:45:29 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <div>
-        <div className="px-4 border border-green-700 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 border bg-green-400 border-green-700 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="p-8 rounded shadow-xl sm:p-16">
             <div className="flex flex-col lg:flex-row">
               <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
@@ -20,9 +20,7 @@ export default function Home() {
                   The quick, brown fox
                   <br className="hidden md:block" />
                   jumps over{" "}
-                  <span className="inline-block text-deep-purple-accent-400">
-                    a lazy dog
-                  </span>
+                  <span className="inline-block  text-red-900">a lazy dog</span>
                 </h2>
               </div>
               <div className="lg:w-1/2">
